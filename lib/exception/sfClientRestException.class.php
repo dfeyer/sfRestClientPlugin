ecc9ca621560b8d884c756853b14fe3da5e7eeed
Dominique Feyer <dominique.feyer@reelpeek.net>
2010-11-30 09:23:24 +0100

    Add client exception

---
+++ lib/exception/sfClientRestException.class.php
@@ -0,0 +1,17 @@
+<?php
+ /**
+  * (c) 2006-2010 Dominique Feyer <dominique.feyer@reelpeek.net>
+  *
+  * For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
+ 
+/**
+ * sfClientRestException is thrown when an internal procedure fails.
+ *
+ * @package    sfClientRestException
+ * @author     Dominique Feyer <dominique.feyer@reelpeek.net>
+ */
+class sfClientRestException extends sfException {
+
+}
