6dcaad282052e10a6b32cc98bc3a32ac9c6ce2c8
Dominique Feyer <dominique.feyer@reelpeek.net>
2010-11-30 09:25:59 +0100

    Add new sfRestClient

--- lib/sfRestClient.class.php
+++ lib/sfRestClient.class.php
@@ -6,37 +6,79 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 /**
- * sfRestClient is a generic RESTful client
+ * sfRestClientAbstract is a generic RESTful client implementation for
+ * Symfony Framework.
  *
  * @package    sfRestClientPlugin
- * @subpackage sfRestClientPlugin
  * @author     Dominique Feyer <dominique.feyer@reelpeek.net>
  */
-class sfRestClient extends sfRestClientAbstract
+class sfRestClient implements sfRestClientInterface
 {
+  protected $options;
+
   /**
-   * Unserialize the response to prepare the array $this->response with the proper value
+   * Class constructor.
    *
+   * @param   array  $options           An array of configuration parameters
+   *
+   * @throws sfInitializationException  If CURL extension is not loaded
+   * @see initialize()
+   */
+  public function __construct($options = array())
+  {
+    if (!in_array('curl', get_loaded_extensions()))
+    {
+      throw new sfClientRestException('Check your PHP configuration this plugin require CURL extension');
+    }
+    $this->initialize($options);
+  }
+
+  /**
+   * Initializes this medialibAutoLogin instance.
+   *
+   * @param   string $url               The URL of the remote webservice
+   * @param   array  $options           An array of configuration parameters
+   * @param   array  $data              An array containing URL paramter (parameter name as key name)
+   */
+  protected function initialize($options)
+  {
+    $this->setOptions($options);
+  }
+
+  public static function create($options = array()) {
+    $class = __CLASS__;
+    return new $class($options);
+  }
+
+  /**
+   * Client options setter
+   *
+   * The setter merge the provided array with the default value
+   *
+   * @param   array $options            The configuration paramter
    * @return  sfRestClientAbstract      Current intance of sfRestClientAbstract
    */
-  protected function unserialize()
+  protected function setOptions($options)
   {
-    $this->payload = $this->getSerializer()->unserialize($this->responseBody);
+    $this->options = array_merge(array(
+      'implementation' => sfConfig::get('app_sfRestClient_client_ressource', 'curl')
+    ), $options);
 
     return $this;
   }
 
   /**
-   * Unserialize the response to prepare the array $this->response with the proper value
+   * Return a web ressource based on the current implementation
    *
-   * @return string serialized array ready to be pushed to web service
+   * The setter merge the provided array with the default value
+   *
+   * @param   array $options            The configuration paramter
+   * @return  sfRestClientAbstract      Current intance of sfRestClientAbstract
    */
-  protected function serialize()
+  public function resource($url)
   {
-    return $this->getSerializer()->serialize($this->payload);
+    return sfRestWebResource::getInstance($url, $this->options['implementation']);
   }
 }
-
-?>
