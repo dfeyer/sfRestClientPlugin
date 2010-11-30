f779b1a88cb7cb61d26a64238318737ac499bdb6
Dominique Feyer <dominique.feyer@reelpeek.net>
2010-11-30 10:27:06 +0100

    Add mediatype class

---
+++ lib/sfRestMediaType.class.php
@@ -0,0 +1,23 @@
+<?php
+ /**
+  * (c) 2006-2010 Dominique Feyer <dominique.feyer@reelpeek.net>
+  *
+  * For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
+ 
+/**
+ * Resume
+ *
+ * This class ...
+ *
+ * @package    sfRestMediaType
+ * @author     Dominique Feyer <dominique.feyer@reelpeek.net>
+ */
+class sfRestMediaType
+{
+  const APPLICATION_XML = 'application/xml';
+  const APPLICATION_JSON = 'application/json';
+  const TEXT_HTML = 'text/html';
+  const TEXT_XML = 'text/xml';
+}
